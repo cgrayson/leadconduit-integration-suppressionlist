be56a0a9ebbe7407801784ce47a73fafd9f0a922
Zalary Young <zalary@gmail.com>
2014-10-10 15:41:58 -0500

    add delete item to module exports

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   outbound: {
     query_item: require('./lib/query_item'),
-    add_item: require('./lib/add_item')
+    add_item: require('./lib/add_item'),
+    delete_item: require('./lib/delete_item')
   }
 }
